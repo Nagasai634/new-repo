7c7fab9a4f8240174fa4d3041c34e5fe99c89d8d
nagasai <nagasaikonderu@gmail.com>
2025-08-04 16:36:13 +0530

    push

--- pipeline.groovy
+++ pipeline.groovy
@@ -27,3 +27,51 @@ pipeline {
         }
     }
 }
+
+
+
+
+
+
+
+pipeline {
+    agent {
+        label 'jenkins-slave1' // Specify the agent to run the pipeline
+    }
+    tools {
+        jdk 'jdk' // Define the JDK tool
+        maven 'maven' // Define the Maven tool
+    }
+    environment {
+        APPLICATION_NAME = 'eureka' // Set the application name
+        SONAR_URL = 'http://34.122.117.31:9000' // Set the SonarQube URL
+        SONAR_TOKEN = credentials('sonar_creds') // Retrieve SonarQube token from Jenkins credentials
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo "Building ${APPLICATION_NAME}..." // Log the build process
+                sh "mvn clean package -DskipTests=true" // Build the application, skipping tests
+            }
+        }
+        stage('Code Quality') {
+            steps {
+                echo "Scanning ${APPLICATION_NAME} code for quality..." // Log the code quality scan
+                withSonarQube('sonarqube') { // Use the SonarQube server defined in Jenkins
+                    sh """
+                    mvn clean verify sonar:sonar \
+                      -Dsonar.projectKey=eureka-ms \
+                      -Dsonar.host.url=${SONAR_URL} \
+                      -Dsonar.login=${SONAR_TOKEN} // Run SonarQube analysis
+                    """
+                }
+                timeout(time: 2, unit: "MINUTES") { // Set a timeout for the quality gate check
+                    script {
+                        waitforQualityGate abortPipeline: true // Wait for the quality gate to pass or abort
+                    }
+                }
+            }
+        }
+       
+    }
+}
